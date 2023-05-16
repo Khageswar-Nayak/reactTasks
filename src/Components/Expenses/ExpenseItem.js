414e49070844400d88f7d16e898d100ece2a0ada
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-05-16 20:11:55 +0530

    Uses of React States

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,40 +1,35 @@
-import React from 'react';
-import Card from '../UI/Card';
+import React, { useState } from "react";
+import Card from "../UI/Card";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
-
 const ExpenseItem = (props) => {
   // return React.createElement('Card' , {className : 'expense-item'},
   //  React.createElement(ExpenseDate , {date : props.date} ),
   //  React.createElement(ExpenseDetails , {title : props.title , amount : props.amount, place : props.place} ),
-   
-  //  )
 
-  const handleDelete = () => {
-    const cardElement = document.querySelector('.expense-item');
-    if (cardElement) {
-      cardElement.remove();
-    }
+  //  )
+  const [amount, setAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    setAmount("100$");
+    //console.log(title);
   };
 
-   return (
-    
+  return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} amount={props.amount} place={props.place}/>
+      <ExpenseDetails title={props.title} amount={amount} place={props.place} />
       {/* <li>
         {props.title} {props.amount} {props.place}{" "}
       </li> */}
 
       {/* <p>spend in {props.location}</p> */}
-      <button onClick={handleDelete}>Delete Expense</button>
+      <button onClick={clickHandler}>change Title</button>
     </Card>
- );
-
-  
-}
+  );
+};
 
 export default ExpenseItem;
